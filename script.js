23615ccc3188383cbe75347c7d1a1e7ad081ebfa
Alice Veselá <alice.hladikova@gmail.com>
2024-05-11 20:52:19 +0200

    pridani stylu about us

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const buttons = document.querySelectorAll('.heading-button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    document.querySelectorAll('.container__text').forEach((container) => {
+      container.classList.toggle('hidden');
+    });
+  });
+});
