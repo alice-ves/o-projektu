ce6ae1519ca9c13275d3ba9490003b5d5eaa3955
Alice Veselá <alice.hladikova@gmail.com>
2024-05-13 20:27:20 +0200

    oprava stylovani, pridani linku na github

--- script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('.heading-button');
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    document.querySelectorAll('.container__text').forEach((container) => {
-      container.classList.toggle('hidden');
-    });
-  });
-});
